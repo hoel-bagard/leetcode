3726c68ac5c692bf79b595586f28a9e77d41d484
Hoel Bagard <hoel.bagard@gmail.com>
2023-04-08 15:52:23 +0900

    Added binary search solution.

--- src/0004_Median_of_Two_Sorted_Arrays/src/median.cpp
+++ src/0004_Median_of_Two_Sorted_Arrays/src/median.cpp
@@ -1,25 +1,56 @@
-#include "median.hpp"
+#include <bits/stdc++.h>
+using namespace std;
 
 #include <algorithm>
 #include <vector>
 
-double Solution::findMedianSortedArrays(std::vector<int>& nums1,
-                                        std::vector<int>& nums2) {
-  // Initialization some neccessary variables
-  std::vector<int> v;
+#include "median.hpp"
 
-  // store the array in the new array
-  for (auto num : nums1)  // O(n1)
-    v.push_back(num);
+double Solution::findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+  // Swap to make nums1 the smaller of the two arrays.
+  if (nums1.size() > nums2.size()) return Solution::findMedianSortedArrays(nums2, nums1);
 
-  for (auto num : nums2)  // O(n2)
-    v.push_back(num);
+  long unsigned int length_smaller = nums1.size();
+  long unsigned int length_longer = nums2.size();
 
-  // Sort the array to find the median
-  sort(v.begin(), v.end());  // O(nlogn)
+  long unsigned int start = 0;
+  long unsigned int end = length_smaller;
+  long unsigned int mid_in_merged_array = (length_smaller + length_longer + 1) / 2;
 
-  // Find the median and Return it
-  long unsigned int n = v.size();  // O(n)
+  while (start <= end) {
+    // Compute the number of elements contributed to the left partition from each input array.
+    long unsigned int mid = (start + end) / 2;
+    long unsigned int left_nums1_size = mid;
+    long unsigned int left_nums2_size = mid_in_merged_array - mid;
 
-  return n % 2 ? v[n / 2] : (v[n / 2 - 1] + v[n / 2]) / 2.0;
-};
+    // Check for indices overflow.
+    int left_nums1 = (left_nums1_size > 0) ? nums1[left_nums1_size - 1] : INT_MIN;
+    int left_nums2 = (left_nums2_size > 0) ? nums2[left_nums2_size - 1] : INT_MIN;
+    int right_nums1 = (left_nums1_size < length_smaller) ? nums1[left_nums1_size] : INT_MAX;
+    int right_nums2 = (left_nums2_size < length_longer) ? nums2[left_nums2_size] : INT_MAX;
+
+    // If the partition is correct, then return, otherwise continue the binary search by updating start or end.
+    if (left_nums1 <= right_nums2 and left_nums2 <= right_nums1) {
+      if ((length_smaller + length_longer) % 2 == 0)
+        return (max(left_nums1, left_nums2) + min(right_nums1, right_nums2)) / 2.0;
+      return max(left_nums1, left_nums2);
+    } else if (left_nums1 <= right_nums2) {
+      start = mid + 1;
+    } else
+      end = mid - 1;
+  }
+
+  return 0.0;
+}
+
+// double Solution::findMedianSortedArrays(std::vector<int>& nums1,
+//                                         std::vector<int>& nums2) {
+//   // Merge the arrays and sort the merged array.
+//   std::vector<int> v;
+//   for (auto num : nums1) v.push_back(num);
+//   for (auto num : nums2) v.push_back(num);
+//   sort(v.begin(), v.end());
+
+//   long unsigned int n = v.size();
+//   return n % 2 ? v[n / 2] : (v[n / 2 - 1] + v[n / 2]) / 2.0;
+// };
