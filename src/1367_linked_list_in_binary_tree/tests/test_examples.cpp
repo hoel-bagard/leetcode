7d427fb1fd5dd52df1569bcca863d0c415117181
Hoel Bagard <hoel.bagard@gmail.com>
2023-04-09 14:54:35 +0900

    Added fixture.

--- src/1367_linked_list_in_binary_tree/tests/test_examples.cpp
+++ src/1367_linked_list_in_binary_tree/tests/test_examples.cpp
@@ -8,7 +8,12 @@
 #include "solution.hpp"
 #include "test_utils.hpp"
 
-TEST(MedianTest, Example1) {
+class SolutionExampleTests : public ::testing::Test {
+ protected:
+  Solution solution;
+};
+
+TEST_F(SolutionExampleTests, Example1) {
   {
     std::vector<int> head = {1, 4, 2, 6};
     std::vector<std::optional<int>> root = {1, 4, 4, std::nullopt, 2, 2, std::nullopt, 1, std::nullopt, 6, 8, std::nullopt, std::nullopt, std::nullopt, std::nullopt, 1, 3};
@@ -16,12 +21,11 @@ TEST(MedianTest, Example1) {
     ListNode* linked_list_head = vector_to_linked_list(head);
     TreeNode* tree_root = vector_to_b_tree(root);
 
-    Solution solution_object;
-    EXPECT_TRUE(solution_object.isSubPath(linked_list_head, tree_root));
+    EXPECT_TRUE(solution.isSubPath(linked_list_head, tree_root));
   }
 }
 
-TEST(MedianTest, Example2) {
+TEST_F(SolutionExampleTests, Example2) {
   {
     std::vector<int> head = {1, 4, 2, 6};
     std::vector<std::optional<int>> root = {1, 4, 4, std::nullopt, 2, 2, std::nullopt, 1, std::nullopt, 6, 8, std::nullopt, std::nullopt, std::nullopt, std::nullopt, 1, 3};
@@ -29,7 +33,6 @@ TEST(MedianTest, Example2) {
     ListNode* linked_list_head = vector_to_linked_list(head);
     TreeNode* tree_root = vector_to_b_tree(root);
 
-    Solution solution_object;
-    EXPECT_TRUE(solution_object.isSubPath(linked_list_head, tree_root));
+    EXPECT_TRUE(solution.isSubPath(linked_list_head, tree_root));
   }
 }
--- src/1367_linked_list_in_binary_tree/tests/test_extra.cpp
+++ src/1367_linked_list_in_binary_tree/tests/test_extra.cpp
@@ -8,7 +8,12 @@
 #include "solution.hpp"
 #include "test_utils.hpp"
 
-TEST(MedianTest, ShouldFail1) {
+class SolutionExtraTests : public ::testing::Test {
+ protected:
+  Solution solution;
+};
+
+TEST_F(SolutionExtraTests, ShouldFail1) {
   {
     std::vector<int> head = {1, 4, 2, 21};
     std::vector<std::optional<int>> root = {1, 4, 4, std::nullopt, 2, 2, std::nullopt, 1, std::nullopt, 6, 8, std::nullopt, std::nullopt, std::nullopt, std::nullopt, 1, 3};
@@ -16,12 +21,11 @@ TEST(MedianTest, ShouldFail1) {
     ListNode* linked_list_head = vector_to_linked_list(head);
     TreeNode* tree_root = vector_to_b_tree(root);
 
-    Solution solution_object;
-    EXPECT_FALSE(solution_object.isSubPath(linked_list_head, tree_root));
+    EXPECT_FALSE(solution.isSubPath(linked_list_head, tree_root));
   }
 }
 
-TEST(MedianTest, ShouldFail2) {
+TEST_F(SolutionExtraTests, ShouldFail2) {
   {
     std::vector<int> head = {1, 10};
     std::vector<std::optional<int>> root = {1, std::nullopt, 1, 10, 1, 9};
@@ -29,12 +33,11 @@ TEST(MedianTest, ShouldFail2) {
     ListNode* linked_list_head = vector_to_linked_list(head);
     TreeNode* tree_root = vector_to_b_tree(root);
 
-    Solution solution_object;
-    EXPECT_TRUE(solution_object.isSubPath(linked_list_head, tree_root));
+    EXPECT_TRUE(solution.isSubPath(linked_list_head, tree_root));
   }
 }
 
-TEST(MedianTest, ShouldFail3) {
+TEST_F(SolutionExtraTests, ShouldFail3) {
   {
     std::vector<int> head = {2, 2, 1};
     std::vector<std::optional<int>> root = {2, std::nullopt, 2, std::nullopt, 2, std::nullopt, 1};
@@ -42,7 +45,6 @@ TEST(MedianTest, ShouldFail3) {
     ListNode* linked_list_head = vector_to_linked_list(head);
     TreeNode* tree_root = vector_to_b_tree(root);
 
-    Solution solution_object;
-    EXPECT_TRUE(solution_object.isSubPath(linked_list_head, tree_root));
+    EXPECT_TRUE(solution.isSubPath(linked_list_head, tree_root));
   }
 }
