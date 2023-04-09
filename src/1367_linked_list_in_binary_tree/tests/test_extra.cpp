4ece0daf98c7e4f46a8847df3d611ba5fd463d1a
Hoel Bagard <hoel.bagard@gmail.com>
2023-04-09 15:16:10 +0900

    Made the extra tests into parameterized tests.

--- src/1367_linked_list_in_binary_tree/tests/test_extra.cpp
+++ src/1367_linked_list_in_binary_tree/tests/test_extra.cpp
@@ -2,49 +2,41 @@
 #include <unistd.h>
 
 #include <chrono>
+#include <functional>
 #include <tuple>
 #include <vector>
 
 #include "solution.hpp"
 #include "test_utils.hpp"
 
-class SolutionExtraTests : public ::testing::Test {
+class SolutionFixture : public ::testing::Test {
  protected:
   Solution solution;
 };
 
-TEST_F(SolutionExtraTests, ShouldFail1) {
-  {
-    std::vector<int> head = {1, 4, 2, 21};
-    std::vector<std::optional<int>> root = {1, 4, 4, std::nullopt, 2, 2, std::nullopt, 1, std::nullopt, 6, 8, std::nullopt, std::nullopt, std::nullopt, std::nullopt, 1, 3};
+class SolutionExtraTests
+    : public SolutionFixture,
+      public ::testing::WithParamInterface<std::tuple<std::vector<int>, std::vector<std::optional<int>>, bool>> {};
 
-    ListNode* linked_list_head = vector_to_linked_list(head);
-    TreeNode* tree_root = vector_to_b_tree(root);
+TEST_P(SolutionExtraTests, CheckNotInTree) {
+  std::vector<int> head = std::get<0>(GetParam());
+  std::vector<std::optional<int>> root = std::get<1>(GetParam());
+  bool expected = std::get<2>(GetParam());
 
-    EXPECT_FALSE(solution.isSubPath(linked_list_head, tree_root));
-  }
+  ListNode* linked_list_head = vector_to_linked_list(head);
+  TreeNode* tree_root = vector_to_b_tree(root);
+
+  ASSERT_EQ(expected, solution.isSubPath(linked_list_head, tree_root));
 }
 
-TEST_F(SolutionExtraTests, ShouldFail2) {
-  {
-    std::vector<int> head = {1, 10};
-    std::vector<std::optional<int>> root = {1, std::nullopt, 1, 10, 1, 9};
-
-    ListNode* linked_list_head = vector_to_linked_list(head);
-    TreeNode* tree_root = vector_to_b_tree(root);
-
-    EXPECT_TRUE(solution.isSubPath(linked_list_head, tree_root));
-  }
-}
-
-TEST_F(SolutionExtraTests, ShouldFail3) {
-  {
-    std::vector<int> head = {2, 2, 1};
-    std::vector<std::optional<int>> root = {2, std::nullopt, 2, std::nullopt, 2, std::nullopt, 1};
-
-    ListNode* linked_list_head = vector_to_linked_list(head);
-    TreeNode* tree_root = vector_to_b_tree(root);
-
-    EXPECT_TRUE(solution.isSubPath(linked_list_head, tree_root));
-  }
-}
+INSTANTIATE_TEST_SUITE_P(
+    SolutionExtraTests, SolutionExtraTests,
+    ::testing::Values(
+        std::make_tuple(std::vector<int>{1, 4, 2, 21},
+                        std::vector<std::optional<int>>{1, 4, 4, std::nullopt, 2, 2, std::nullopt, 1, std::nullopt, 6,
+                                                        8, std::nullopt, std::nullopt, std::nullopt, std::nullopt, 1,
+                                                        3},
+                        false),
+        std::make_tuple(std::vector<int>{1, 10}, std::vector<std::optional<int>>{1, std::nullopt, 1, 10, 1, 9}, true),
+        std::make_tuple(std::vector<int>{2, 2, 1},
+                        std::vector<std::optional<int>>{2, std::nullopt, 2, std::nullopt, 2, std::nullopt, 1}, true)));
